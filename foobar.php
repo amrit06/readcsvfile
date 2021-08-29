6fb4d582727c69d0cfda73887a4e326db5afddd5
Amrit Kharel <amrit.kharel6@gmail.com>
2021-08-29 10:45:13 +1000

    comment addeed

--- foobar.php
+++ foobar.php
@@ -3,9 +3,10 @@
 
 <?php
 for ($i = 1; $i < 101; $i++) {
-    $value = "{$i}, ";
+    $value = "{$i}, "; // value
 
-    if ($i % 3 == 0 || $i % 5 == 0) {
+    if ($i % 3 == 0 || $i % 5 == 0) { 
+        // if value isdivisible by 3 0r 5 set value = "" and print foor or bar accordingly
         $value = "";
         if ($i % 3 == 0) {
             $value .= "foo";
