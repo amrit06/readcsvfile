61bb92e225678e4372d31823125032e53af738bb
Amrit Kharel <amrit.kharel6@gmail.com>
2021-08-29 10:28:58 +1000

    add to db completed

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,7 @@
 #!/bin/php
 
+
+
 <?php
 class UserTask
 {
@@ -12,7 +14,6 @@ class UserTask
         $result =  ucfirst(strtolower($result));
         return $result;
     }
-
 
     public function readFromCSV($file)
     {
@@ -35,42 +36,66 @@ class UserTask
         }
     }
 
-
-    public function saveToDB($username, $password, $host, $dbname)
+    public function createDB($host, $username, $password, $dbname)
     {
-        $tablename = "users";
-        $conn = new mysqli($host, $username, $password, $dbname);
-
+        $conn = new mysqli($host, $username, $password);
         if ($conn->connect_error) {
             echo "Connection failed: " . $conn->connect_error;
             exit(1);
         }
 
-        $testQuery = "SELECT * FROM " . $tablename;
-        if ($conn->query($testQuery)) {
-            // exists
+        $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA
+        WHERE SCHEMA_NAME = '{$dbname}'";
+        if (!$conn->query($query)) {
+            $query = "CREATE DATABASE IF NOT EXISTS " . $dbname;
+            if ($conn->query($query)) {
+                echo "DB created... \n";
+            }
+        }
+        $conn->close();
+    }
 
-        } else {
-            // doesn't exit
+    public function saveToDB($username, $password, $host)
+    {
+        $dbname = "useruploadDB";
+        $tablename = "users";
+
+        //create DB
+        $this->createDB($host, $username, $password, $dbname);
+
+        //check connection
+        $conn = new mysqli($host, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            echo "Connection failed: " . $conn->connect_error;
+            $conn->close();
+            exit(1);
+        }
+
+        //check if table exists if not create it
+        $testQuery = "SELECT * FROM " . $tablename;
+        if (!$conn->query($testQuery)) { // runs only when table isn't present
+            //create table
             $query =
-                "CREATE TABLE users (
+                "CREATE TABLE {$tablename} (
             name varchar(255) NOT NULL,
             surname varchar(255) NOT NULL,
             email varchar(255) NOT NULL)";
             $conn->query($query);
+            echo  "Table Created... \n";
         }
 
+        // save csv data to db
         foreach ($this->userData as $items) {
             $query =
-                "INSERT INTO users 
-                    (name, surname, email) 
-                    VALUES 
-                    ({$items['name']}, {$items['surname']}, {$items['email']})";
+                "INSERT INTO {$tablename} (name, surname, email) VALUES
+                 ('{$items['name']}', '{$items['surname']}',' {$items['email']}');";
             $conn->query($query);
         }
+        echo "Complete inserting \n";
+        $conn->close();
     }
 
-    public function dryRun()
+    public function printOut()
     {
         print("\nName\t\t" . "Surname\t\t" . "Email\n");
         foreach ($this->userData as $items) {
@@ -82,12 +107,20 @@ class UserTask
 ?>
 
 <?php
+//things that are assumed
+// Db is already setup
+// Db name and table name is provided
+// --file directive is executed with either --create_table or --dry_run
+// if --create_table is used -u -h -p is also passed
+
+
 $usertask = new UserTask();
 
+// get directives and their values
 $vars = getopt("u:p:h:", ["file:", "create_table::", "dry_run::", "help::"]);
 
 print("***********************************************\n");
-if (array_key_exists("help", $vars)) {
+if (array_key_exists("help", $vars)) { // case help
     print("This Script allows you to pass csv file along with MySQL info and save data \n" .
         "\n" .
         "\n" .
@@ -105,20 +138,23 @@ if (array_key_exists("help", $vars)) {
     exit(0);
 }
 
+
 if (array_key_exists("file", $vars) && array_key_exists("create_table", $vars)) {
+    // case create table
     if (array_key_exists("u", $vars) && array_key_exists("h", $vars) && array_key_exists("p", $vars)) {
+        // make sure you have username, host and password.
         $usertask->readFromCSV($vars["file"]);
-        $usertask->saveToDB($vars["u"], $vars["p"], $vars["h"], "csvDB");
+        $usertask->saveToDB($vars["u"], $vars["p"], $vars["h"]);
     } else {
         print("please refer to help. press --help \n");
     }
 } else if (array_key_exists("file", $vars) && array_key_exists("dry_run", $vars)) {
+    //dry_run
     $usertask->readFromCSV($vars["file"]);
-    $usertask->dryRun();
+    $usertask->printOut();
 } else {
     print("please refer to help. press --help \n");
 }
 print("***********************************************\n");
-
 
 ?>
